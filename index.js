82c71d7a4a1aed8ae4c46ad0a4f66247b2621d99
spaceboundbear <mf.michaelfischer@gmail.com>
2021-10-05 16:44:25 -0500

    file structure and index code

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const path = require('path');
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const output = path.resolve(__dirname, 'output', 'output.html');
+
 const emp = [];
 
-function prompt() {
+function init() {
   inquirer
     .prompt([
       {
@@ -36,6 +38,7 @@ function prompt() {
       let { name, id, email, number } = answers;
       let manager = Manager(name, id, email, number);
       emp.push(manager);
+      console.info('Successfully Added Manager!');
       addTeam();
     });
 }
@@ -45,14 +48,14 @@ function addTeam() {
     .prompt([
       {
         type: 'list',
-        name: 'addMembers',
-        message: 'Add Team Member?',
+        name: 'addMember',
+        message: 'Add A Team Member?',
         choices: ['Add Engineer', 'Add Intern', 'Done Adding Team Members'],
       },
     ])
 
     .then((answers) => {
-      eSwitch = answers.addMembers;
+      eSwitch = answers.addMember;
 
       switch (eSwitch) {
         case 'Add Engineer':
@@ -69,3 +72,75 @@ function addTeam() {
       }
     });
 }
+
+function addEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Enter Engineer Name: ',
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: 'Enter Engineer ID: ',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter Engineer Email: ',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter Engineer Github Username',
+      },
+    ])
+    .then((answers) => {
+      var { name, id, email, github } = answers;
+      var engineer = Engineer(name, id, email, github);
+      emp.push(engineer);
+      console.info('Successfully Added Engineer!');
+      addTeam();
+    });
+}
+
+function addIntern() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Enter Intern Name: ',
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: 'Enter Intern ID: ',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter Intern Email: ',
+      },
+      {
+        type: 'input',
+        name: 'school',
+        message: 'Enter Intern School Name: ',
+      },
+    ])
+    .then((answers) => {
+      var { name, id, email, school } = answers;
+      var intern = Intern(name, id, email, school);
+      emp.push(intern);
+      console.info('Successfully Added Intern!');
+      addTeam();
+    });
+}
+
+function makeTeam() {
+  fs.writeFileSync(output, renderMain(emp), 'utf-8');
+}
+
+init();
